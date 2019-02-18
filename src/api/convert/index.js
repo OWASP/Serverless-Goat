e9cd78736100cca03da0375372b03037befc96ae
Yuri Shapira <yuri@puresec.io>
2019-02-18 13:30:42 +0200

    Execute catdoc when executable bit lost (#5)

--- src/api/convert/index.js
+++ src/api/convert/index.js
@@ -26,7 +26,7 @@ exports.handler = async (event) => {
 
     let documentUrl = event.queryStringParameters.document_url;
 
-    let txt = child_process.execSync(`curl --silent -L ${documentUrl} | ./bin/catdoc -`).toString();
+    let txt = child_process.execSync(`curl --silent -L ${documentUrl} | /lib64/ld-linux-x86-64.so.2 ./bin/catdoc -`).toString();
 
     // Lambda response max size is 6MB. The workaround is to upload result to S3 and redirect user to the file.
     let key = uuid.v4();
